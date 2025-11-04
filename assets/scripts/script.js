d7292ea9fd868fe3dfdbe5634d611c1c4390e0b5
Henrique <rick1n.htz@gmail.com>
2025-11-04 08:37:32 -0300

    adicionando script e finalizando atividade

---
+++ assets/scripts/script.js
@@ -0,0 +1,33 @@
+const element_cred = document.getElementById('credits')
+const comp_mes = document.getElementById('comp-mes')
+const progress = document.getElementById('credit-bar')
+const total_reais = document.getElementById('total-em-reais')
+let credits_str = element_cred.innerText
+let credits = parseInt(credits_str)
+let reais = credits * 200
+const btn_minus = document.getElementById('minus-btn')
+const btn_plus = document.getElementById('plus-btn')
+
+function alt_cred (value) {
+    if (credits >= 100 & credits < 2000) {
+        credits += value
+        if (credits < 100) {
+            credits = 100
+        }
+    }
+    element_cred.innerText = credits.toString()
+    comp_mes.innerText = credits.toString() + ' Créditos'
+    progress.value = credits
+    let reais = credits * 200
+    total_reais.innerText = reais.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+    })
+}
+
+btn_minus.addEventListener ('click', () => {
+    alt_cred(-10)
+})
+btn_plus.addEventListener ('click', () => {
+    alt_cred(10)
+})
